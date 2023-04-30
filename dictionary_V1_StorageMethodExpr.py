f11c44f397d0f8401a112b1a60cf5cf1ca34d995
jesusjesusjesusjesus <cartero@middleton.school.nz>
2023-05-01 10:05:55 +1200

    yup

---
+++ dictionary_V1_StorageMethodExpr.py
@@ -0,0 +1,19 @@
+filled_out = (1, 0)
+monster_1 = {"ID": 1, "name": "Stoneling", "strength": 7, "speed": 1, "stealth": 25, "cunning": 15, filled_out: 1}
+monster_2 = {"ID": 2, "name": "Vexscream", "strength": 1, "speed": 6, "stealth": 21, "cunning": 19, filled_out: 1}
+monster_3 = {"ID": 3, "name": "Dawnmirage", "strength": 5, "speed": 15, "stealth": 18, "cunning": 22, filled_out: 1}
+monster_4 = {"ID": 4, "name": "Blazegolem", "strength": 15, "speed": 20, "stealth": 23, "cunning": 6, filled_out: 1}
+monster_5 = {"ID": 5, "name": "Websnake", "strength": 7, "speed": 15, "stealth": 10, "cunning": 5, filled_out: 1}
+monster_6 = {"ID": 6, "name": "Moldvine", "strength": 21, "speed": 18, "stealth": 14, "cunning": 5, filled_out: 1}
+monster_7 = {"ID": 7, "name": "Vortexwing", "strength": 19, "speed": 13, "stealth": 19, "cunning": 2, filled_out: 1}
+monster_8 = {"ID": 8, "name": "Rotthing", "strength": 16, "speed": 7, "stealth": 4, "cunning": 12, filled_out: 1}
+monster_9 = {"ID": 9, "name": "Froststep", "strength": 14, "speed": 14, "stealth": 17, "cunning": 4, filled_out: 1}
+monster_10 = {"ID": 10, "name": "Wispghoul", "strength": 17, "speed": 19, "stealth": 3, "cunning": 2, filled_out: 1}
+monster_11 = {"ID": 11, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0}
+monster_12 = {"ID": 12, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0}
+monster_13 = {"ID": 13, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0}
+monster_14 = {"ID": 14, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0}
+monster_15 = {"ID": 15, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0}
+monsters_list = [monster_1, monster_2, monster_3, monster_4, monster_5, monster_6, monster_7, monster_8, monster_9,
+                 monster_10, monster_11, monster_12, monster_13, monster_14, monster_15]
+
