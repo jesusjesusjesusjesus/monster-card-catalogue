bc340998d3e7cbc1bc97f3224868ae06e362351f
jesusjesusjesusjesus <cartero@middleton.school.nz>
2023-05-02 12:15:27 +1200

    Create dictionary_V2_StorageMethodExpr.py

---
+++ dictionary_V2_StorageMethodExpr.py
@@ -0,0 +1,18 @@
+filled_out = (1, 0)
+monsters = {1:
+            {"ID": 1, "name": "Stoneling", "strength": 7, "speed": 1, "stealth": 25, "cunning": 15, filled_out: 1},
+            2: {"ID": 2, "name": "Vexscream", "strength": 1, "speed": 6, "stealth": 21, "cunning": 19, filled_out: 1},
+            3: {"ID": 3, "name": "Dawnmirage", "strength": 5, "speed": 15, "stealth": 18, "cunning": 22, filled_out: 1},
+            4: {"ID": 4, "name": "Blazegolem", "strength": 15, "speed": 20, "stealth": 23, "cunning": 6, filled_out: 1},
+            5: {"ID": 5, "name": "Websnake", "strength": 7, "speed": 15, "stealth": 10, "cunning": 5, filled_out: 1},
+            6: {"ID": 6, "name": "Moldvine", "strength": 21, "speed": 18, "stealth": 14, "cunning": 5, filled_out: 1},
+            7: {"ID": 7, "name": "Vortexwing", "strength": 19, "speed": 13, "stealth": 19, "cunning": 2, filled_out: 1},
+            8: {"ID": 8, "name": "Rotthing", "strength": 16, "speed": 7, "stealth": 4, "cunning": 12, filled_out: 1},
+            9: {"ID": 9, "name": "Froststep", "strength": 14, "speed": 14, "stealth": 17, "cunning": 4, filled_out: 1},
+            10: {"ID": 10, "name": "Wispghoul", "strength": 17, "speed": 19, "stealth": 3, "cunning": 2, filled_out: 1},
+            11: {"ID": 11, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0},
+            12: {"ID": 12, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0},
+            13: {"ID": 13, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0},
+            14: {"ID": 14, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0},
+            15: {"ID": 15, "name": "", "strength": 0, "speed": 0, "stealth": 0, "cunning": 0, filled_out: 0}
+            }
